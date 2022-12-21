dc6d1b3c9069b5e42cd26060b8360f688622261f
ches85 <349044123@gapps.yrdsb.ca>
2022-12-21 14:08:27 -0500

    Adding in the world

--- MyWorld.java
+++ MyWorld.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class MyWorld here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @ Brady
+ * @ Version 1
  */
 public class MyWorld extends World
 {
